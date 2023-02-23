d627a72743985e2d5b7c2fba494c74342ceb2b2e
Bengisu Kandemir <bkandemi@student.hive.fi>
2023-02-23 17:00:15 +0200

    err check router run added

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/bengissimo/tictactoe/pkg/game"
 )
 
 func main() {
 	gs := game.NewStore()
 
-	gs.Router.Run("0.0.0.0:8080")
+	if err := gs.Router.Run("0.0.0.0:8080"); err != nil {
+		log.Fatal(err)
+	}
 }
